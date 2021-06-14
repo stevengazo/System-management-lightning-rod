75091885a2145b5ef6e826424801d1b8128c657f
Steven Gazo M <43178863+stevengazo@users.noreply.github.com>
2021-06-13 20:43:33 -0600

    cambio de datacontext = identity

--- Control/Startup.cs
+++ Control/Startup.cs
@@ -32,12 +32,12 @@ namespace Control
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddDbContext<Data.IDBContext>(options =>
+            services.AddDbContext<IDBContext>(options =>
                 options.UseSqlServer(
                     Configuration.GetConnectionString("DefaultConnection")));
 
             services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
-                .AddEntityFrameworkStores<Data.IDBContext>();
+                .AddEntityFrameworkStores<IDBContext>();
 
             services.AddRazorPages();
             services.AddServerSideBlazor();
