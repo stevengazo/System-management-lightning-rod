4c7e8509afa76ae0e774ad290030517307b202b6
Steven Gazo M <sfabricio27@live.com>
2021-08-09 12:11:27 -0600

    busqueda de clientes funcional

--- Business/B_Client.cs
+++ Business/B_Client.cs
@@ -14,6 +14,39 @@ namespace Business
     {
         
         
+        /// <summary>
+        /// Search clients in the database
+        /// </summary>
+        /// <param name="_name">Name of the client</param>
+        /// <param name="_Id">Id of the client in the database</param>
+        /// <returns>List of clients</returns>
+        public static List<ClientEntity> SearchClients(string _name = null, string _Id = null)
+        {
+            List<ClientEntity> Clients = new List<ClientEntity>();
+            using( var DB = new RayosNoDataContext())
+            {
+                if( (_name != null) && (_Id!= null))
+                {
+                    var aux = DB.Clients.FromSqlInterpolated($@"SELECT * FROM Clients
+                                                                WHERE	(Id like Concat('%',{_Id},'%'))
+                                                                and		(Name like concat('%',{_name},'%'))");
+                    Clients = aux.ToList();
+                }
+                else if ((_name == null) && (_Id != null))
+                {
+                    var aux = DB.Clients.FromSqlInterpolated($@"SELECT * FROM Clients
+                                                                WHERE	(Id like Concat('%',{_Id},'%'))");
+                    Clients = aux.ToList();
+                }
+                else if ((_name != null) && (_Id == null))
+                {
+                    var aux = DB.Clients.FromSqlInterpolated($@"SELECT * FROM Clients
+                                                                WHERE	(Name like concat('%',{_name},'%'))");
+                    Clients = aux.ToList();
+                }
+                return Clients;
+            }
+        }
         public static List<ClientEntity> SearchClientsByName(string Name = "")
         {
             using(var DB= new RayosNoDataContext())
