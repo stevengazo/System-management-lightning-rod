4f2207387852072cb268b76a4826826fbda1c8ad
Steven Gazo M <sfabricio27@live.com>
2021-08-13 17:32:53 -0600

    inicio de ingreso de documentos creación de clase

---
+++ Entities/DocumentEntity.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace Entities
+{
+    public class DocumentEntity
+    {
+        [Key]
+        [Required]
+        public string DocumentId { get; set; }
+        [Required]
+        public string TypeOfDocument { get; set; }
+        
+        public string Author { get; set; }
+        [Required]
+        public string Path { get; set; }
+        [Required]
+        public DateTime DateOfCreation { get; set; }
+    }
+}
