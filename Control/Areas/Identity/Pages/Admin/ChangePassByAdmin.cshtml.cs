0ede251be58b35c30006f4b467ca5ce17bf767fd
Steven Gazo M <43178863+stevengazo@users.noreply.github.com>
2021-11-07 14:44:01 -0600

    pruebas

---
+++ Control/Areas/Identity/Pages/Admin/ChangePassByAdmin.cshtml.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Control.Areas.Identity.Pages.Admin
+{
+    public class ChangePassByAdminModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
