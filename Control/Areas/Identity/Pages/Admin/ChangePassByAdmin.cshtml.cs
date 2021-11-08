40e8772800bce59c1bffe1c0a28891b26ed5af1a
Steven Gazo M <43178863+stevengazo@users.noreply.github.com>
2021-11-07 18:20:19 -0600

    Change pass by admin en progreso

--- Control/Areas/Identity/Pages/Admin/ChangePassByAdmin.cshtml.cs
+++ Control/Areas/Identity/Pages/Admin/ChangePassByAdmin.cshtml.cs
@@ -8,6 +8,7 @@ using Microsoft.AspNetCore.Identity;
 using Control.Areas.Identity.Pages.Account;
 using Microsoft.Extensions.Logging;
 using IdentityDataAccess;
+using System.ComponentModel.DataAnnotations;
 
 namespace Control.Areas.Identity.Pages.Admin
 {
@@ -18,36 +19,128 @@ namespace Control.Areas.Identity.Pages.Admin
         private readonly ILogger<LoginModel> _logger;
         private readonly IDBContext _iDBContext;
 
+
+        public class InputModel
+        {
+            [DataType(DataType.Text)]
+            [Display(Name = "User Id")]
+            public string idUser { get; set; }
+
+            [DataType(DataType.Password)]
+            [Display(Name = "New password")]
+            public string NewPassword { get; set; }
+
+            [DataType(DataType.Password)]
+            [Display(Name = "Confirm new password")]
+            [Compare("NewPassword", ErrorMessage = "The new password and confirmation password do not match.")]
+            public string ConfirmPassword { get; set; }
+        }
+
+
         public ChangePassByAdminModel(SignInManager<IdentityUser> signInManager,
             ILogger<LoginModel> logger,
             UserManager<IdentityUser> userManager,
             IDBContext _iDBContext )
         {
-            this._userManager = _userManager;
-            this._signInManager = _signInManager;
-            this._logger = _logger;
+            this._signInManager = signInManager;
+            this._logger = logger;
+            this._userManager = userManager;
             this._iDBContext = _iDBContext;
         }
 
 
 
-        [Route("/Identity/Admin/ChangePassByAdmin/{id}")]
+        private async Task<bool>  ChangePassword(IdentityUser user, string _password)
+        {
+            IdentityResult removepass;
+            IdentityResult changepassword;
+            try
+            {
+                if(user != null)
+                {
+                    removepass=  this._userManager.RemovePasswordAsync(user).Result;
+                    if (removepass.Succeeded)
+                    {
+                        changepassword =  this._userManager.AddPasswordAsync(user, _password).Result;
+                        if (changepassword.Succeeded)
+                        {
+                            return true;
+                        }
+                        else
+                        {
+                            return false;
+                        }
+                    }
+                    return false;
+                }
+
+                return true;
+            }
+            catch (Exception)
+            {
+
+                return false;
+            }
+        }
+
+
+        private IdentityUser GetUser(string _id = "")
+        {
+            try
+            {
+                using (var db = new IDBContext())
+                {
+                    var query = (from us in db.Users select us).FirstOrDefault(U => U.Id == _id);
+                    return query;
+                }
+                return null;
+            }
+            catch (Exception f)
+            {
+                Console.WriteLine($"Error: {f.Message}");
+                return null;
+            }
+        }
+
+      
+        [HttpGet]
         public async Task<IActionResult> OnGetAsync(string id="")
         {
             IdentityUser user = new IdentityUser();
-            using (var db = new IDBContext())
-            {
-                var query = (from us in db.Users select us).FirstOrDefault(U=>U.Id== id);
-                user = query;
-            }
-
+            user = GetUser(id);
             ViewData["User"] = user;
             return Page();
         }
 
-        public async Task<IActionResult> OnPostAsync(string id = "")
+        [HttpPost]
+        public async Task<IActionResult> OnPostAsync(string id = "", string password="",string cpassword = "")
         {
-            ViewData["sample"] = id;
+            bool BandSuccess = false ;
+            var user = GetUser(id);
+            string error = "";
+
+            if( (password == null) || (cpassword == null))
+            {
+
+                error = "Verifica que hayas introducido información";
+            }
+            else
+            {
+                if (!(password.Equals(cpassword)))
+                {
+                    error = "La Contraseña no es identica, verifica los datos";
+                }
+                else
+                {
+                   BandSuccess= await ChangePassword(user,password);
+                   if (BandSuccess)
+                   {
+                        error = "Contraseña cambiada";
+                   }
+                }
+            }                               
+            ViewData["User"] = user;
+            ViewData["error"] = error;
             return Page();
         }
     }
