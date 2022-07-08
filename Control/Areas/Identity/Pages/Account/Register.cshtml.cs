4ee68ed32142e588997ebaa2bc9402f8811e0d1c
Steven Gazo M <43178863+stevengazo@users.noreply.github.com>
2022-07-08 17:16:46 -0600

    cambios menores y nueva conexión

--- Control/Areas/Identity/Pages/Account/Register.cshtml.cs
+++ Control/Areas/Identity/Pages/Account/Register.cshtml.cs
@@ -68,14 +68,26 @@ namespace Control.Areas.Identity.Pages.Account
 
         public async Task OnGetAsync(string returnUrl = null)
         {
+            _ = (ExistDB()) ? ViewData["FlagDB"] = 0 : ViewData["FlagDB"] = 1;
+      
             ViewData["FistUse"] = NotExistsUsers();
+         
             ReturnUrl = returnUrl;
             ExternalLogins = (await _signInManager.GetExternalAuthenticationSchemesAsync()).ToList();
             
         }
 
+
+        private bool ExistDB()
+        {
+            var sample = _IDBContext.Database.CanConnect();
+            return sample;
+        }
+
+
         public async Task<IActionResult> OnPostAsync(string returnUrl = null)
         {
+            
             returnUrl ??= Url.Content("~/");
             ExternalLogins = (await _signInManager.GetExternalAuthenticationSchemesAsync()).ToList();
             if (ModelState.IsValid)
